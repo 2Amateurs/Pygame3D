8c12f70860b754791ac611827ff8aa6fa8950fda
Sam <71229731+2Amateurs@users.noreply.github.com>
2022-06-03 12:28:52 -0400

    Add files via upload

--- pygame3D.py
+++ pygame3D.py
@@ -37,21 +37,17 @@ class objectTypes(enum.Enum):
     terrain = 2
     enemy = 3
 class point:
-    def __init__(self, x, y, z): 
+    def __init__(self, x, y, z, parent): 
         self.x = x
         self.y = y
         self.z = z
         self.xRelative = 0
         self.yRelative = 0
         self.zRelative = 0
-        self.yPitchVector = 0
-        self.zPitchVector = 0
-        self.yawHypotenuse = 0
-        self.adjacent = 0
-        self.xProjected = 0
-        self.yProjected = 0
-        self.thetaPitch = 0
-        self.thetaYaw = 0
+        self.rotationValues = [0, 0, 0, 0] #1: x 2: z 3: z 4: y
+        self.myParent = parent
+        self.myParent.pointRegistry.append(self)
+        self.myIndex = len(self.myParent.pointRegistry)-1
     def update(self):
        self.xRelative = self.x-renderData.camX
        self.yRelative = self.y-renderData.camY
@@ -67,26 +63,19 @@ class point:
         else:
             return value/abs(value)
     def rotateAroundCam(self, pitch, yaw):
-        self.yPitchVector = self.yRelative * math.cos(pitch)
-        self.zPitchVector = self.yRelative * math.sin(pitch)
-        self.yawHypotenuse = math.sqrt((self.xRelative**2)+(self.zRelative**2)) * self.mult(self.zRelative)
-        if self.zRelative == 0:
-            self.thetaYaw = 1.5708
-        else:
-            self.thetaYaw = math.atan(self.xRelative/self.zRelative) + yaw
-        self.adjacent = self.yawHypotenuse * math.cos(self.thetaYaw)
-        self.thetaPitch = pitch + 1.5708
-        self.xRelative = self.yawHypotenuse * math.sin(self.thetaYaw)
-        self.yRelative = self.adjacent * math.cos(self.thetaPitch) + self.yPitchVector
-        self.zRelative = self.adjacent * math.sin(self.thetaPitch) + self.zPitchVector
+        self.rotationValues[0] = self.xRelative*math.cos(-yaw) - self.zRelative*math.sin(-yaw)
+        self.rotationValues[1] = self.xRelative*math.sin(-yaw) + self.zRelative*math.cos(-yaw)
+        self.rotationValues[2] = self.rotationValues[1]*math.cos(-pitch) - self.yRelative*math.sin(-pitch)
+        self.rotationValues[3]  = self.rotationValues[1]*math.sin(-pitch) + self.yRelative*math.cos(-pitch)
+        self.xRelative = self.rotationValues[0]
+        self.yRelative = self.rotationValues[3]
+        self.zRelative = self.rotationValues[2]
         return self
     def projectTo2D(self):
-        if self.zRelative <= 1:#A temporary solution that I need to fix
-            self.xProjected = 0#None
-            self.yProjected = 0#None
-        else:
-            self.xProjected = renderData.screenWidth/2 + (renderData.viewbox*self.xRelative)/((self.zRelative+1)*renderData.FOVconst)
-            self.yProjected = renderData.screenHeight/2 - (renderData.viewbox*self.yRelative)/((self.zRelative+1)*renderData.FOVconst)
+        self.xProjected = renderData.screenWidth/2 + (renderData.viewbox*self.xRelative)/((self.zRelative)*renderData.FOVconst)
+        self.yProjected = renderData.screenHeight/2 - (renderData.viewbox*self.yRelative)/((self.zRelative)*renderData.FOVconst)
+        if self.zRelative < 0:
+            self.xProjected = None
 class cuboidRegistry:
     def __init__(self):
         self.registry = []
@@ -108,14 +97,15 @@ class cuboid:
         self.width = width
         self.height = height
         self.depth = depth
-        self.point1 = point(0, 0, 0)
-        self.point2 = point(0, 0, 0)
-        self.point3 = point(0, 0, 0)
-        self.point4 = point(0, 0, 0)
-        self.point5 = point(0, 0, 0)
-        self.point6 = point(0, 0, 0)
-        self.point7 = point(0, 0, 0)
-        self.point8 = point(0, 0, 0)
+        self.pointRegistry = []
+        self.point1 = point(0, 0, 0, self)#passes the self parameter so each point can easily access the parent object
+        self.point2 = point(0, 0, 0, self)
+        self.point3 = point(0, 0, 0, self)
+        self.point4 = point(0, 0, 0, self)
+        self.point5 = point(0, 0, 0, self)
+        self.point6 = point(0, 0, 0, self)
+        self.point7 = point(0, 0, 0, self)
+        self.point8 = point(0, 0, 0, self)
         self.setPoints()
         self.objectType = objectType
         cuboidRegistry.registry.append(self)
@@ -124,29 +114,34 @@ class cuboid:
         self.y = y
         self.z = z
         self.setPoints()
+    def setupDisplay(self):
+        for item in self.pointRegistry:
+            item.update().rotateAroundCam(renderData.pitch, renderData.yaw).projectTo2D()
+    def drawLine(self, x1, y1, x2, y2, color):
+        if not((x1 == None) or (x2 == None)):
+            pygame.draw.line(renderData.screen, color, (x1, y1), (x2, y2), 3)
     def display(self, color):
-        self.point1.update().rotateAroundCam(renderData.pitch, renderData.yaw,).projectTo2D()
-        self.point2.update().rotateAroundCam(renderData.pitch, renderData.yaw,).projectTo2D()
-        self.point3.update().rotateAroundCam(renderData.pitch, renderData.yaw,).projectTo2D()
-        self.point4.update().rotateAroundCam(renderData.pitch, renderData.yaw,).projectTo2D()
-        self.point5.update().rotateAroundCam(renderData.pitch, renderData.yaw,).projectTo2D()
-        self.point6.update().rotateAroundCam(renderData.pitch, renderData.yaw,).projectTo2D()
-        self.point7.update().rotateAroundCam(renderData.pitch, renderData.yaw,).projectTo2D()
-        self.point8.update().rotateAroundCam(renderData.pitch, renderData.yaw,).projectTo2D()
-        pygame.draw.line(renderData.screen, color, (self.point1.xProjected, self.point1.yProjected), (self.point2.xProjected, self.point2.yProjected), 3)
-        pygame.draw.line(renderData.screen, color, (self.point2.xProjected, self.point2.yProjected), (self.point3.xProjected, self.point3.yProjected), 3)
-        pygame.draw.line(renderData.screen, color, (self.point3.xProjected, self.point3.yProjected), (self.point4.xProjected, self.point4.yProjected), 3)
-        pygame.draw.line(renderData.screen, color, (self.point4.xProjected, self.point4.yProjected), (self.point1.xProjected, self.point1.yProjected), 3)
-        pygame.draw.line(renderData.screen, color, (self.point5.xProjected, self.point5.yProjected), (self.point6.xProjected, self.point6.yProjected), 3)
-        pygame.draw.line(renderData.screen, color, (self.point6.xProjected, self.point6.yProjected), (self.point7.xProjected, self.point7.yProjected), 3)
-        pygame.draw.line(renderData.screen, color, (self.point7.xProjected, self.point7.yProjected), (self.point8.xProjected, self.point8.yProjected), 3)
-        pygame.draw.line(renderData.screen, color, (self.point8.xProjected, self.point8.yProjected), (self.point5.xProjected, self.point5.yProjected), 3)
-        pygame.draw.line(renderData.screen, color, (self.point1.xProjected, self.point1.yProjected), (self.point5.xProjected, self.point5.yProjected), 3)
-        pygame.draw.line(renderData.screen, color, (self.point2.xProjected, self.point2.yProjected), (self.point6.xProjected, self.point6.yProjected), 3)
-        pygame.draw.line(renderData.screen, color, (self.point3.xProjected, self.point3.yProjected), (self.point7.xProjected, self.point7.yProjected), 3)
-        pygame.draw.line(renderData.screen, color, (self.point4.xProjected, self.point4.yProjected), (self.point8.xProjected, self.point8.yProjected), 3)
+        self.drawLine(self.point1.xProjected, self.point1.yProjected, self.point2.xProjected, self.point2.yProjected, color)
+        self.drawLine(self.point2.xProjected, self.point2.yProjected, self.point3.xProjected, self.point3.yProjected, color)
+        self.drawLine(self.point3.xProjected, self.point3.yProjected, self.point4.xProjected, self.point4.yProjected, color)
+        self.drawLine(self.point4.xProjected, self.point4.yProjected, self.point1.xProjected, self.point1.yProjected, color)
+        self.drawLine(self.point5.xProjected, self.point5.yProjected, self.point6.xProjected, self.point6.yProjected, color)
+        self.drawLine(self.point6.xProjected, self.point6.yProjected, self.point7.xProjected, self.point7.yProjected, color)
+        self.drawLine(self.point7.xProjected, self.point7.yProjected, self.point8.xProjected, self.point8.yProjected, color)
+        self.drawLine(self.point8.xProjected, self.point8.yProjected, self.point5.xProjected, self.point5.yProjected, color)
+        self.drawLine(self.point1.xProjected, self.point1.yProjected, self.point5.xProjected, self.point5.yProjected, color)
+        self.drawLine(self.point2.xProjected, self.point2.yProjected, self.point6.xProjected, self.point6.yProjected, color)
+        self.drawLine(self.point3.xProjected, self.point3.yProjected, self.point7.xProjected, self.point7.yProjected, color)
+        self.drawLine(self.point4.xProjected, self.point4.yProjected, self.point8.xProjected, self.point8.yProjected, color)
     def contact(self, objectX, objectY, objectZ, objectWidth, objectHeight, objectDepth):
         return (objectX + objectWidth/2 > self.x-self.width/2) and (objectX - objectWidth/2 < self.x+self.width/2) and (objectY + objectHeight/2 > self.y-self.height/2) and (objectY - objectHeight/2 < self.y+self.height/2) and (objectZ + objectDepth/2 > self.z-self.depth/2) and (objectZ - objectDepth/2 < self.z+self.depth/2)
+def touching(hitbox):
+    touching = False
+    for item in cuboidRegistry.registry:
+        if item.contact(hitbox.x, hitbox.y, hitbox.z, hitbox.width, hitbox.height, hitbox.depth) and not (item.objectType == 'player' or item is hitbox):
+            touching = True
+    return touching
 def displayObjects(color, objectType):
     for item in cuboidRegistry.registry:
+        item.setupDisplay()
         item.display(color)
